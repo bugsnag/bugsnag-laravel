bc8ea403287ee592c420ec8cc77f348156435a93
James Smith <james@loopj.com>
2013-11-09 15:57:50 -0800

    Skeleton

---
+++ src/Bugsnag/BugsnagLaravel/BugsnagLaravelServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Bugsnag\BugsnagLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class BugsnagLaravelServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('bugsnag/bugsnag-laravel');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
