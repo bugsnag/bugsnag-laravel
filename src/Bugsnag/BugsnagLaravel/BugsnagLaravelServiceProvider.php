e9e58c849f0926c615b7f96c5c8c1bb94d35ca25
James Smith <james@loopj.com>
2013-11-11 14:34:39 -0800

    Fix code style

--- src/Bugsnag/BugsnagLaravel/BugsnagLaravelServiceProvider.php
+++ src/Bugsnag/BugsnagLaravel/BugsnagLaravelServiceProvider.php
@@ -2,66 +2,65 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class BugsnagLaravelServiceProvider extends ServiceProvider {
+class BugsnagLaravelServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('bugsnag/bugsnag-laravel', 'bugsnag');
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('bugsnag/bugsnag-laravel', 'bugsnag');
+        $app = $this->app;
 
-		$app = $this->app;
+        // Register for exception handling
+        $app->error(function (\Exception $exception) use ($app) {
+            $app['bugsnag']->notifyException($exception);
+        });
 
-		// Register for exception handling
-		$app->error(function(\Exception $exception) use ($app) {
-			$app['bugsnag']->notifyException($exception);
-		});
+        // Register for fatal error handling
+        // TODO: For some reason, Bugsnag_Client#shutdownHandler is never called
+        $app->fatal(function ($exception) use ($app) {
+            $app['bugsnag']->notifyException($exception);
+        });
+    }
 
-		// Register for fatal error handling
-		// TODO: For some reason, Bugsnag_Client#shutdownHandler is never called
-		$app->fatal(function($exception) use ($app) {
-			$app['bugsnag']->notifyException($exception);
-		});
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('bugsnag', function ($app) {
+            $config = $app['config']['bugsnag'] ?: $app['config']['bugsnag::config'];
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->singleton('bugsnag', function($app) {
-			$config = $app['config']['bugsnag'] ?: $app['config']['bugsnag::config'];
+            $client = new \Bugsnag_Client($config['api_key']);
 
-			$client = new \Bugsnag_Client($config['api_key']);
+            //Set releaseStage, etc from config if present
 
-			//Set releaseStage, etc from config if present
+                    $client->setReleaseStage($this->app->environment());
 
-            		$client->setReleaseStage($this->app->environment());
+            return $client;
+        });
+    }
 
-			return $client;
-		});
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array("bugsnag");
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array("bugsnag");
+    }
 }
