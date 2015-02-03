ca081de477001871fada2563f8629ffb569b47ea
Conrad Irwin <conrad.irwin@gmail.com>
2015-02-03 12:44:44 -0800

    Add laravel-4 example

---
+++ example/laravel-4/app/config/packages/bugsnag/bugsnag-laravel/config.php
@@ -0,0 +1,7 @@
+<?php return array(
+    /**
+     * Set your Bugsnag API Key.
+     * You can find your API Key on your Bugsnag dashboard.
+     */
+    'api_key' => 'bb81bec11e5a8da0cf35a78364f56890'
+);
---
+++ example/laravel-4/app/views/hello.php
@@ -0,0 +1,11 @@
+Homepage!
+
+<?php
+
+var_dump($app['bugsnag']);
+
+throw new Exception("Something broke");
+
+// $app['bugsnag']->notifyError("Hello", "world");
+
+?>
