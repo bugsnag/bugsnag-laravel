7eace82682c1d8d739d9d0b130a0c1633bfb0949
Luca Critelli <lucacri@gmail.com>
2015-05-11 15:41:44 -0400

    Lumen example app

---
+++ example/lumen/tests/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class ExampleTest extends TestCase {
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $response = $this->call('GET', '/');
+
+        $this->assertResponseOk();
+    }
+
+}
