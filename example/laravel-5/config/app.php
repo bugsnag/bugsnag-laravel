2667fefce2a8e764da12bceb59f812062546d27a
Simon Maynard <snmaynard@gmail.com>
2015-07-11 07:29:05 +0100

    Update instructions

--- example/laravel-5/config/app.php
+++ example/laravel-5/config/app.php
@@ -145,7 +145,7 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 
-		'Bugsnag\BugsnagLaravel\BugsnagLaravelServiceProvider',
+		Bugsnag\BugsnagLaravel\BugsnagLaravelServiceProvider::class,
 
 	],
 
@@ -166,7 +166,7 @@ return [
 		'Artisan'   => 'Illuminate\Support\Facades\Artisan',
 		'Auth'      => 'Illuminate\Support\Facades\Auth',
 		'Blade'     => 'Illuminate\Support\Facades\Blade',
-		'Bugsnag'   => 'Bugsnag\BugsnagLaravel\BugsnagLaravelFacade',
+		'Bugsnag'   => Bugsnag\BugsnagLaravel\BugsnagLaravelFacade::class,
 		'Bus'       => 'Illuminate\Support\Facades\Bus',
 		'Cache'     => 'Illuminate\Support\Facades\Cache',
 		'Config'    => 'Illuminate\Support\Facades\Config',
