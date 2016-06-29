80e744d6f404f940b1f1ef3ab43f7a840dbe8892
Graham Campbell <graham@bugsnag.com>
2016-06-29 17:32:06 +0100

    Use symfony's path method

--- src/Request/LaravelRequest.php
+++ src/Request/LaravelRequest.php
@@ -93,7 +93,7 @@ class LaravelRequest implements RequestInterface
      */
     public function getContext()
     {
-        return $this->request->getMethod().' '.$this->request->getUri();
+        return $this->request->getMethod().' '.$this->request->getPathInfo();
     }
 
     /**
