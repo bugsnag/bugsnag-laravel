518bae21e2ab92fc9defdc3a205c839ba5b911fe
Graham Campbell <graham@alt-three.com>
2016-08-17 10:01:56 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- src/BugsnagServiceProvider.php
+++ src/BugsnagServiceProvider.php
@@ -3,8 +3,8 @@
 namespace Bugsnag\BugsnagLaravel;
 
 use Bugsnag\Breadcrumbs\Breadcrumb;
-use Bugsnag\BugsnagLaravel\Request\LaravelResolver;
 use Bugsnag\BugsnagLaravel\Queue\Tracker;
+use Bugsnag\BugsnagLaravel\Request\LaravelResolver;
 use Bugsnag\Callbacks\CustomUser;
 use Bugsnag\Client;
 use Bugsnag\Configuration;
@@ -107,8 +107,8 @@ class BugsnagServiceProvider extends ServiceProvider
             $this->app->bugsnag->setAppType('Queue');
 
             $job = [
-                'attempts'   => $event->job->attempts(),
-                'queue'      => $event->job->getQueue(),
+                'attempts' => $event->job->attempts(),
+                'queue' => $event->job->getQueue(),
                 'connection' => $event->connectionName,
             ];
 
