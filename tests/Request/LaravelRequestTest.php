bfff938adc5e3d6f4f08046643aa6b523330e9ae
Delisa Mason <kattrali@users.noreply.github.com>
2017-08-21 07:56:53 -0700

    Apply fixes from StyleCI (#247)

--- tests/Request/LaravelRequestTest.php
+++ tests/Request/LaravelRequestTest.php
@@ -2,10 +2,9 @@
 
 namespace Bugsnag\BugsnagLaravel\Tests\Request;
 
-use Bugsnag\BugsnagLaravel\Request\LaravelRequest;
 use Bugsnag\BugsnagLaravel\Request\ConsoleRequest;
+use Bugsnag\BugsnagLaravel\Request\LaravelRequest;
 use Bugsnag\BugsnagLaravel\Request\LaravelResolver;
-use Bugsnag\Request\NullRequest;
 use Bugsnag\Request\RequestInterface;
 use GrahamCampbell\TestBenchCore\MockeryTrait;
 use Illuminate\Foundation\Application;
