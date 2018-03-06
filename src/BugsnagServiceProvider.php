daa7adfef1dac1a7e1ed7913b23eadc9d49f626f
Delisa Mason <iskanamagus@gmail.com>
2018-03-06 10:47:13 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/BugsnagServiceProvider.php
+++ src/BugsnagServiceProvider.php
@@ -3,13 +3,12 @@
 namespace Bugsnag\BugsnagLaravel;
 
 use Bugsnag\Breadcrumbs\Breadcrumb;
+use Bugsnag\BugsnagLaravel\MultiLogger as BaseMultiLogger;
 use Bugsnag\BugsnagLaravel\Queue\Tracker;
 use Bugsnag\BugsnagLaravel\Request\LaravelResolver;
 use Bugsnag\Callbacks\CustomUser;
 use Bugsnag\Client;
 use Bugsnag\Configuration;
-use Bugsnag\BugsnagLaravel\LaravelLogger;
-use Bugsnag\BugsnagLaravel\MultiLogger as BaseMultiLogger;
 use Bugsnag\Report;
 use Illuminate\Auth\GenericUser;
 use Illuminate\Contracts\Container\Container;
