30669b0297797302d68bea40f3e3291df59ecec2
Martin Bean <martinbean@users.noreply.github.com>
2018-07-05 15:46:54 +0100

    Add phpDoc method tags
    Synthesise client methods on façade to enable autocomplete in IDEs that read phpDoc tags.

--- src/Facades/Bugsnag.php
+++ src/Facades/Bugsnag.php
@@ -4,6 +4,24 @@ namespace Bugsnag\BugsnagLaravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Bugsnag\Client make(string|null $apiKey, string|null $endpoint, bool $defaults)
+ * @method static \GuzzleHttp\ClientInterface make(string|null $base, array $options)
+ * @method static string|false getCaBundlePath()
+ * @method static \Bugsnag\Configuration getConfig()
+ * @method static $this registerCallback(callable $callback)
+ * @method static $this registerDefaultCallbacks()
+ * @method static void leaveBreadcrumb(string $name, string|null $type, array $metaData)
+ * @method static void clearBreadcrumbs()
+ * @method static void notifyException(\Throwable $throwable, callable|null $callback)
+ * @method static void notifyError(string $name, string $message, callable|null $callback)
+ * @method static void notify(\Bugsnag\Report $report, callable|null $callback)
+ * @method static void deploy(string|null $repository, string|null $branch, string|null $revision)
+ * @method static void build(string|null $repository, string|null $revision, string|null $provider, string|null $builderName)
+ * @method static void flush()
+ * @method static void startSession()
+ * @method static \Bugsnag\SessionTracker getSessionTracker()
+ */
 class Bugsnag extends Facade
 {
     protected static function getFacadeAccessor()
