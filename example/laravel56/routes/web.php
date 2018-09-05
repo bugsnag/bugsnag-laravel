61cdf1ff3084658ed1d6159b9b411e3da4db1745
Alex Moinet <alex@bugsnag.com>
2018-09-05 16:19:58 +0100

    Dockerisation: More example routes/navigation

--- example/laravel56/routes/web.php
+++ example/laravel56/routes/web.php
@@ -15,6 +15,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('test', function () {
-    throw new Exception('Example exception!');
+Route::get('crash', function () {
+    throw new Exception('Crashing exception!');
+});
+
+Route::get('notify', function () {
+    Bugsnag::notifyException(new Exception('A notified exception'));
+    return "An exception has been notified.  Check your dashboard to view it!";
+});
+
+Route::get('metadata', function () {
+    Bugsnag::registerCallback(function($report) {
+        $report->setMetaData([
+            'routeDetails' => [
+                'path' => 'metadata',
+                'function' => 'Adds a callback to Bugsnag that adds metadata to a notification'
+            ]
+        ]);
+    });
+    throw new Exception('Metadata exception!');
+});
+
+Route::get('notifywithmetadata', function () {
+    Bugsnag::notifyException(new Exception('A notified exception'), function($report) {
+        $report->setMetaData([
+            'routeDetails' => [
+                'path' => 'notifywithmetadata',
+                'function' => 'Shows how to send extra data with a manually notified exception!'
+            ]
+        ]);
+    });
+    return "An exception has been notified.  Check your dashboard to view it!";
+});
+
+Route::get('severity', function () {
+    Bugsnag::notifyException(new Exception('A notified exception'), function($report) {
+        $report->setSeverity("info");
+    });
+    return "An exception has been notified.  Check your dashboard to view it!";
 });
