f04044f3b0baaa2daca8d7fc8b34aa8bdf3c4e69
Alex Moinet <alex@bugsnag.com>
2018-10-23 11:56:17 +0100

    Maze-tests: Moved shared server steps into maze-runner

--- features/support/env.rb
+++ features/support/env.rb
@@ -14,13 +14,3 @@ def install_current_branch(fixture)
     `cat composer.json.base | sed 's/TEST_BRANCH_NAME/#{branch_name}/g' > composer.json`
   end
 end
-
-def current_ip
-  if OS.mac?
-    'host.docker.internal'
-  else
-    ip_addr = `ifconfig | grep -Eo 'inet (addr:)?([0-9]*\\\.){3}[0-9]*' | grep -v '127.0.0.1'`
-    ip_list = /((?:[0-9]*\.){3}[0-9]*)/.match(ip_addr)
-    ip_list.captures.first
-  end
-end
