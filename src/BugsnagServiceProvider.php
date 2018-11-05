a14b85233c6b22ad96dfe5daaf3d370309e12296
Delisa Mason <iskanamagus@gmail.com>
2018-11-05 09:58:06 -0800

    Release v2.15.1

--- src/BugsnagServiceProvider.php
+++ src/BugsnagServiceProvider.php
@@ -35,7 +35,7 @@ class BugsnagServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '2.15.0';
+    const VERSION = '2.15.1';
 
     /**
      * Boot the service provider.
