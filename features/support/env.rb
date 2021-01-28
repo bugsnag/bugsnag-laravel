d92387e9477f5fa0fbf2864a008496137c7ae6fd
Joe Haines <hello@joehaines.co.uk>
2021-01-28 15:48:17 +0000

    Clean up more thoroughly after running MR

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,31 +3,39 @@ require 'json'
 
 # Copy bugsnag-laravel into fixture directories
 FIXTURE_DIR = 'features/fixtures'
-VENDORED_LIB = FIXTURE_DIR + '/bugsnag-laravel.zip'
+VENDORED_LIB = "#{FIXTURE_DIR}/bugsnag-laravel.zip"
+FILES_TO_CLEANUP = [VENDORED_LIB]
 
 `composer archive -f zip --dir=#{File.dirname(VENDORED_LIB)} --file=#{File.basename(VENDORED_LIB, '.zip')}`
-Dir.glob(FIXTURE_DIR + '/laravel*').each do |directory|
+
+Dir.glob("#{FIXTURE_DIR}/laravel*").each do |directory|
   next if directory.end_with?('laravel-latest')
 
-  FileUtils.cp(VENDORED_LIB, directory + '/bugsnag-laravel.zip')
-  # Remove any locally installed composer deps
-  FileUtils.rm_rf(directory + '/vendor')
-end
+  zip = "#{directory}/bugsnag-laravel.zip"
+  FILES_TO_CLEANUP << zip
 
+  FileUtils.cp(VENDORED_LIB, zip)
+
+  # Remove any locally installed composer deps
+  FileUtils.rm_rf("#{directory}/vendor")
+end
 
 # Copy current requirements into fixture requirements
 File.open('composer.json', 'r') do |source|
   parsed_composer = JSON.parse(source.read)
   requirements = parsed_composer["require"]
 
-  Dir.glob(FIXTURE_DIR + '/laravel*').each do |directory|
+  Dir.glob("#{FIXTURE_DIR}/laravel*").each do |directory|
     next if directory.end_with?('laravel-latest')
 
     File.open(directory + '/composer.json.template', 'r') do |template|
       parsed_template = JSON.parse template.read
       parsed_template["repositories"][0]["package"]["require"] = requirements
 
-      File.open(directory + '/composer.json', 'w') do |target|
+      composer_json = "#{directory}/composer.json"
+      FILES_TO_CLEANUP << composer_json
+
+      File.open(composer_json, 'w') do |target|
         target.write(JSON.pretty_generate(parsed_template))
       end
     end
@@ -41,7 +49,11 @@ Before do
 end
 
 at_exit do
-  FileUtils.rm_rf(VENDORED_LIB)
+  project_root = File.dirname(File.dirname(__dir__))
+
+  FILES_TO_CLEANUP.each do |file|
+    FileUtils.rm_rf("#{project_root}/#{file}")
+  end
 end
 
 AfterConfiguration do |_config|
