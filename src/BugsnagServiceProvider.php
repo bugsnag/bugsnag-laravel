c61d0d309cd46858b6d0070a8f3846739668d13f
Joe Haines <hello@joehaines.co.uk>
2021-04-28 17:18:41 +0100

    Bump version to 2.22.1

--- src/BugsnagServiceProvider.php
+++ src/BugsnagServiceProvider.php
@@ -36,7 +36,7 @@ class BugsnagServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    const VERSION = '2.22.0';
+    const VERSION = '2.22.1';
 
     /**
      * Boot the service provider.
