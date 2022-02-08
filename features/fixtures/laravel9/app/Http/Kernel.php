23095c3bfe267449ee66012a778f7fa3ef9c14f8
Joe Haines <hello@joehaines.co.uk>
2022-02-08 15:57:14 +0000

    Disable sessions in Laravel 9 fixture

--- features/fixtures/laravel9/app/Http/Kernel.php
+++ features/fixtures/laravel9/app/Http/Kernel.php
@@ -32,10 +32,11 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
+            // Removed to test https://github.com/bugsnag/bugsnag-laravel/pull/476
+            // \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            // \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
