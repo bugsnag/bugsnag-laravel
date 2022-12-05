71065e3150bb02439a0b3251a79d82bbb3ce8642
Joe Haines <hello@joehaines.co.uk>
2022-12-05 12:23:34 +0000

    Run queue tests with Laravel 6

---
+++ features/fixtures/laravel66/app/Jobs/UnhandledJob.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use RuntimeException;
+
+class UnhandledJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public int $tries;
+
+    public function __construct(int $tries)
+    {
+        $this->tries = $tries;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        throw new RuntimeException('uh oh :o');
+    }
+}
