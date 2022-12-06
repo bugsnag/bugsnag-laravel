ee79bceaa0329a77aeb6ee6819506de0c48db37a
Joe Haines <hello@joehaines.co.uk>
2022-12-06 13:09:01 +0000

    Don't pretend the database path is configurable
    The database file is created in the dockerfiles so cannot be changed
    
    The Laravel 5.1 fixture is already not configurable

--- features/fixtures/laravel58/config/database.php
+++ features/fixtures/laravel58/config/database.php
@@ -35,7 +35,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => database_path('database.sqlite'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
--- features/fixtures/laravel8/config/database.php
+++ features/fixtures/laravel8/config/database.php
@@ -37,8 +37,8 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'url' => null,
+            'database' => database_path('database.sqlite'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
