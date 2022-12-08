e42744372f755baff54856a621c595e73d25166d
Joe Haines <hello@joehaines.co.uk>
2022-12-08 15:43:11 +0000

    Update to Maze Runner v7.9.0

--- features/steps/laravel_steps.rb
+++ features/steps/laravel_steps.rb
@@ -14,36 +14,22 @@ When(/^I start the laravel fixture$/) do
   }
 end
 
-# TODO: contribute this back to Maze Runner
-#       https://github.com/bugsnag/maze-runner/pull/425
 module Maze
   class Docker
     class << self
+      # TODO: remove when https://github.com/bugsnag/maze-runner/pull/425 is merged
       def exec(service, command, detach: false)
         flags = detach ? "--detach" : ""
 
         run_docker_compose_command("exec #{flags} #{service} #{command}")
       end
 
+      # TODO: contribute this back to Maze Runner
+      #       probably need a nicer API, capable of doing a copy in either
+      #       direction (right now this can only copy from the service to the
+      #       local machine)
       def cp(service, source:, destination:)
-        # cp only exists in 'docker compose' not 'docker-compose', i.e. only
-        # with a space and not a hyphen
-        # TODO: can we run all docker compose commands with a space?
-        run_docker_space_compose_command("cp #{service}:#{source} #{destination}")
-      end
-
-      private
-
-      def get_docker_space_compose_command(command)
-        project_name = compose_project_name.nil? ? '' : "-p #{compose_project_name}"
-
-        "docker compose #{project_name} -f #{COMPOSE_FILENAME} #{command}"
-      end
-
-      def run_docker_space_compose_command(command, success_codes: nil)
-        command = get_docker_space_compose_command(command)
-
-        @last_command_logs, @last_exit_code = Runner.run_command(command, success_codes: success_codes)
+        run_docker_compose_command("cp #{service}:#{source} #{destination}")
       end
     end
   end
